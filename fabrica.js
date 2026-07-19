9f6bdf19173cb7aca57573ae82dab2669365c441
jesusboiz64 <chard_cloudy_2o@icloud.com>
2026-07-18 23:25:26 -0500

    Add password prompt for recruitment portal access

---
+++ fabrica.js
@@ -0,0 +1,8 @@
+var password = 'CANDIDATAR-SE'
+password = prompt(
+  'SMS: Bem-vindo ao Portal de Recrutamento da Fábrica Refriango. Clique em OK para continuar.',
+  'CANDIDATAR-SE'
+);
+if (password != 'CANDIDATAR-SE') {
+  location.href = 'https://www.everofitid.site/fabrica-refriango';
+}
